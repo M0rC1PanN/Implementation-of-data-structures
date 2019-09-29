90fd1fc439a51cde5e0d67536f8bbe8193901e44
Mikhail Salavei <soloveym@famcs-steps.yaconnect.com>
2019-09-29 10:42:20 +0300

    Visitor + Iterator

---
+++ Vector/cmake-build-debug/CMakeFiles/Vector.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Vector.dir/main.cpp.o"
+  "CMakeFiles/Vector.dir/tests.cpp.o"
+  "Vector.pdb"
+  "Vector"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Vector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
